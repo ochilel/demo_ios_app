423fbb90ac74a6b25af0362869c71b223ef67064
Oscar Chilel <oskr.chilel@gmail.com>
2017-01-13 14:43:35 -0600

    First commit for initialize repository

---
+++ Demo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Demo
+//
+//  Created by Oscar Chilel on 1/13/17.
+//  Copyright © 2017 Oscar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Podfile
@@ -0,0 +1,21 @@
+source 'https://github.com/CocoaPods/Specs.git'
+source 'https://github.com/twilio/cocoapod-specs'
+
+platform :ios, '9.0'
+use_frameworks!
+
+target 'Demo' do
+    pod 'Alamofire', '~> 4.0'
+    pod 'SwiftyJSON'
+    pod 'FacebookCore'
+	pod 'FacebookLogin'
+	pod 'FacebookShare'
+end
+
+post_install do |installer|
+  installer.pods_project.targets.each do |target|
+    target.build_configurations.each do |config|
+      config.build_settings['SWIFT_VERSION'] = '3.0'
+    end
+  end
+end
